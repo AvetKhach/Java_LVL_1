800d5e01a2cf8383cf024198e20e82e1be3eff26
AvetKhach <avetav77@gmail.com>
2022-01-20 20:16:13 +0400

    added priitives

---
+++ src/com/company/Homework1/Classwork1/Primitives.java
@@ -0,0 +1,44 @@
+package com.company.Homework1.Classwork1;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        byte by1 = 0b01111111;
+        byte by2 = 0b0111111;
+        byte by3 = 5;
+
+        char c1 = 'a';
+        char c2 = 'b';
+        char c3 = Character.MAX_VALUE;
+
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        int a1 = Integer.MIN_VALUE;
+        int a2 = Integer.MAX_VALUE;
+        int a3 = 45;
+
+        int color = 0b11111111_11111111_00000000_00000000;
+        System.out.println("color" + color);
+
+        long l1 = Long.MIN_VALUE;
+        long l2 = Long.MAX_VALUE;
+        long l3 = 456;
+
+        short s1 = Short.MIN_VALUE;
+        short s2 = Short.MAX_VALUE;
+        short s3 = 32767;
+
+        float f1 = Float.MIN_VALUE;
+        float f2 = Float.MAX_VALUE;
+        float f3 = 1.5646f;
+
+        double d1 = Double.MIN_VALUE;
+        double d2 = Double.MAX_VALUE;
+        double d3 = 658768767867.9876986896;
+
+
+    }
+}
