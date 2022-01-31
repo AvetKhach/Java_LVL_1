bb00eab037c54491c105170e80b3d0216f7f1e16
AvetKhach <avetav77@gmail.com>
2022-01-31 21:36:43 +0400

    User creation

--- src/com/aca/homework/week2/user/creation/UserCreation.java
+++ src/com/aca/homework/week2/user/creation/UserCreation.java
@@ -4,16 +4,16 @@ package com.aca.homework.week2.user.creation;
 public class UserCreation {
     public static void main(String[] args) {
         User user1 = new User();
-        User user2 = new User();
-        User user3 = new User();
         user1.name = "Marry";
         user1.height = 170;
+        printUser(user1);
+        User user2 = new User();
         user2.name = "Grace";
         user2.height = 169;
+        printUser(user2);
+        User user3 = new User();
         user3.name = "Nicolas";
         user3.height = 175;
-        printUser(user1);
-        printUser(user2);
         printUser(user3);
     }
     public static void printUser(User user) {
