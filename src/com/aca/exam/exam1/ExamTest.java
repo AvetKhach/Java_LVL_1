97ebfefeb2e1202b3e479adf088fa68cfa0bdf6b
AvetKhach <avetav77@gmail.com>
2022-02-02 20:27:41 +0400

    Exam 1

---
+++ src/com/aca/exam/exam1/ExamTest.java
@@ -0,0 +1,12 @@
+package com.aca.exam.exam1;
+
+
+
+public class ExamTest {
+
+    public static void main(String[] args) {
+
+        Exam exam = new Exam();
+        exam.includeOptionNumber(exam.include5Numbers());
+    }
+}
