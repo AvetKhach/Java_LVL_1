ef2938c8b0b9a96b3e5989eaeb0b3bf1109c72a4
AvetKhach <avetav77@gmail.com>
2022-02-05 14:20:29 +0400

    Random String

---
+++ src/com/aca/homework/week3/classroom/ACAClass.java
@@ -0,0 +1,12 @@
+package com.aca.homework.week3.classroom;
+
+public class ACAClass {
+
+    Student [] student;
+    long startMillis;
+    long endMillis;
+    String name;
+    int fee;
+
+
+}
---
+++ src/com/aca/homework/week3/classroom/MainACA.java
@@ -0,0 +1,29 @@
+package com.aca.homework.week3.classroom;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainACA {
+    public static void main(String[] args) throws ParseException {
+        String startDate = "01-01-2022 00:00";
+        String endDate = "01-05-2022 00:00";
+
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+
+        Date start = format.parse(startDate);
+        Date end = format.parse(endDate);
+
+
+        ACAClass acaClass = new ACAClass();
+        acaClass.name = "java level 1";
+        acaClass.startMillis = start.getTime();
+        acaClass.endMillis = end.getTime();
+        acaClass.fee = 500000;
+        acaClass.student = new Student[10];
+
+        System.out.println(acaClass.name +" "+ acaClass.startMillis+" "+acaClass.endMillis+" "+acaClass.fee);
+        System.out.println(start+"      "+ end);
+    }
+}
