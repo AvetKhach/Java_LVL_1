8aa31f1a67f9b181dfa673763f2f2b83ccd80748
AvetKhach <avetav77@gmail.com>
2022-02-16 16:12:14 +0400

    Added objects

--- src/com/aca/homework/week2/maxmin/MainMaxMin.java
+++ src/com/aca/homework/week2/maxmin/MainMaxMin.java
@@ -3,13 +3,19 @@ package com.aca.homework.week2.maxmin;
 import java.util.Scanner;
 
 public class MainMaxMin {
+
+
     public static void main(String[] args) {
+        MaxMin maxMin = new MaxMin();
         System.out.println("Include 2 numbers");
-        long first = new Scanner(System.in).nextLong();
-        long second = new Scanner(System.in).nextLong();
-        System.out.println("Max is "+ compareNumbers(first,second).max + "\nMin is "+ compareNumbers(first,second).min);
+        maxMin.max = new Scanner(System.in).nextLong();
+        maxMin.min = new Scanner(System.in).nextLong();
+
+        System.out.println("Max is " + createMaxMin(maxMin.max, maxMin.min).max + "\nMin is " + createMaxMin(maxMin.max, maxMin.min).min);
     }
-    public static MaxMin compareNumbers(long a, long b) {
+
+
+    public static MaxMin createMaxMin(long a, long b) {
         MaxMin maxMin = new MaxMin();
         if (a > b) {
             maxMin.max = a;
--- src/com/aca/homework/week2/maxmin/MaxMin.java
+++ src/com/aca/homework/week2/maxmin/MaxMin.java
@@ -4,4 +4,5 @@ public class MaxMin {
 
     long max;
     long min;
+
 }
