191c32cae6b6a4662ab31aa9394039d9dcee04aa
AvetKhach <avetav77@gmail.com>
2022-02-16 21:53:13 +0400

    The changed method name

--- src/com/aca/homework/week2/random/string/RandomString.java
+++ src/com/aca/homework/week2/random/string/RandomString.java
@@ -5,17 +5,15 @@ import java.util.Random;
 public class RandomString {
     public static void main(String[] args) {
         for (int i = 0; i <10 ; i++) {
-            returnRandomString();
+            createAndPrintRandomString();
         }
     }
-    public static void returnRandomString(){
+    public static void createAndPrintRandomString(){
         String [] array= {"a","b","w","7","8","9","u"};
-        String newArray = "";
+        String randomChars = "";
         for (int i = 0; i <5 ; i++) {
-
-            newArray += array[new Random().nextInt(7)];
+            randomChars +=  array[new Random().nextInt(7)];
         }
-        System.out.println(newArray);
-
+        System.out.println(randomChars);
     }
 }
